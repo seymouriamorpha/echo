012f1036f04990db988f365de2ff9c6e22638228
seymouria <seymouriamorpha@gmail.com>
2017-06-27 04:43:02 +0300

    Initial commit

---
+++ src/main/java/by/seymouria/echo/Main.java
@@ -0,0 +1,26 @@
+package by.seymouria.echo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author seymouria on 27.06.2017.
+ */
+@Controller
+@EnableAutoConfiguration
+public class Main {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
